66a66ac2e0e449d92b670c709c944204d7d1388f
xiaoshuheng <157579405@qq.com>
2020-04-30 17:25:51 +0800

    基类

--- src/tickettrip2/kernel/HttpPost.php
+++ src/tickettrip2/kernel/HttpPost.php
@@ -7,6 +7,16 @@ use wulilele\tickettrip2\kernel\Config;
 class HttpPost
 {
     public static function post($apiPath,$data=array()){
-        return curlPost($apiPath, self::createSign($data));
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $apiPath);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 300);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // 跳过证书检查
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false); // 从证书中检查SSL加密算法是否存在
+        $handles = curl_exec($ch);
+        curl_close($ch);
+        return $handles;
     }
 }
